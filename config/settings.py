2cf9951b2944467d1b465badc0abf02a431e6a72
Ace <baha20_02@inbox.ru>
2024-05-17 22:40:52 +0600

    added

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,16 +80,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'sport',
-        'USER': 'postgres',
-        'PASSWORD': '12341234',
-        'HOST': 'localhost',
-        'PORT': '5432'
-    }
-}
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+} 
 
 
 # Password validation
